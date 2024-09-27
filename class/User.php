e86b6a69626d91fb141a0dccaf23455b7a035aac
pumba250 <158454148+pumba250@users.noreply.github.com>
2024-09-27 20:11:00 +0300

    Update User.php

--- class/User.php
+++ class/User.php
@@ -60,4 +60,16 @@ public function logout() {
     session_destroy();
 }
 }
+function flash(?string $message = null){
+    if ($message) {
+        $_SESSION['flash'] = $message;
+    } else {
+        if (!empty($_SESSION['flash'])) { ?>
+          <div class="w3-text-red">
+              <?=$_SESSION['flash']?>
+          </div>
+        <?php }
+        unset($_SESSION['flash']);
+    }
+}
 ?>
